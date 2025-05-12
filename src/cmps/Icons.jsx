7cd75ca55be144f2f44f92db7209576433cf0ffe
Ido Feffer <idoppr1@gmail.com>
2025-05-13 01:57:49 +0300

    UI/StayIconFilter

--- src/cmps/Icons.jsx
+++ src/cmps/Icons.jsx
@@ -62,3 +62,13 @@ export function VISAicon(){
   )
 }
 
+export function LeftIcon(){
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 12px; width: 12px; fill: currentcolor;"><path d="m10.55.3 1.42 1.4L5.67 8l6.3 6.3-1.42 1.4-6.59-6.58a1.58 1.58 0 0 1-.1-2.13l.1-.11z"/></svg>
+  )
+}
+export function RightIcon(){
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 12px; width: 12px; fill: currentcolor;"><path d="M5.41.3 4 1.7 10.3 8 4 14.3l1.41 1.4 6.6-6.58c.57-.58.6-1.5.1-2.13l-.1-.11z"/></svg>
+  )
+}
--- src/cmps/StayIconFilter.jsx
+++ src/cmps/StayIconFilter.jsx
@@ -1,4 +1,16 @@
+import { useRef } from "react"
+// import { LeftIcon, RightIcon } from "./Icons"
+
 export function StayIconFilter() {
+  const scrollContainerRef = useRef(null)
+
+  function onScrollLeft() {
+    scrollContainerRef.current?.scrollBy({ left: -700, behavior: "smooth" })
+  }
+  function onScrollRight() {
+    scrollContainerRef.current?.scrollBy({ left: 700, behavior: "smooth" })
+  }
+
   const filters = [
     {
       label: "Arctic",
@@ -110,13 +122,180 @@ export function StayIconFilter() {
       iconUrl:
         "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078100/asset_33_nltjxi.png",
     },
+    {
+      label: "Bed & breakfasts",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078107/asset_39_zvjgdv.png",
+    },
+    {
+      label: "Creative spaces",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078107/asset_42_galcb0.png",
+    },
+    {
+      label: "Ski-in/out",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078112/asset_44_oninsx.png",
+    },
+    {
+      label: "Rooms",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078100/asset_35_qmvilw.png",
+    },
+    {
+      label: "Chef kitchen",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078100/asset_32_nkkkyp.png",
+    },
+    {
+      label: "Desert",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076719/asset_30_wmvje2.png",
+    },
+    {
+      label: "Earth homes",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076719/asset_26_cdvesj.png",
+    },
+    {
+      label: "Histociral homes",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076710/asset_23_euvuyo.png",
+    },
+    {
+      label: "Top cities",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076707/asset_6_ykxppv.png",
+    },
+    {
+      label: "Luxe",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076710/asset_19_yaqdxo.png",
+    },
+    {
+      label: "Off-the-grid",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076709/asset_16_druvl0.png",
+    },
+    {
+      label: "Trending",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076710/asset_18_b1aurj.png",
+    },
+    {
+      label: "Golfing",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076707/asset_58_zkeykv.png",
+    },
+    {
+      label: "Farms",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078084/asset_15_ohyulg.png",
+    },
+    {
+      label: "Countryside",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078079/asset_10_je1oeh.png",
+    },
+    {
+      label: "Amazing view",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078076/asset_6_t8vfgp.png",
+    },
+    {
+      label: "Amazing pools",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078074/asset_2_va6d1q.png",
+    },
+    {
+      label: "Shepherd's huts",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078119/asset_53_zzrauf.png",
+    },
+    {
+      label: "Minsus",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078130/asset_57_zjl8z5.png",
+    },
+    {
+      label: "Trulli",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078135/asset_59_yc8wi5.png",
+    },
+    {
+      label: "Casas particulares",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078135/asset_58_cpnm9t.png",
+    },
+    {
+      label: "Cycldic homes",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078135/asset_58_cpnm9t.png",
+    },
+    {
+      label: "Ryokans",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076727/asset_50_eic5th.png",
+    },
+    {
+      label: "OMG!",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076707/asset_8_abswds.png",
+    },
+    {
+      label: "Mansions",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747087389/asset_9_bmsvby.png",
+    },
+    {
+      label: "Beachfront",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747087389/asset_8_fxggjr.png",
+    },
+    {
+      label: "Cabins",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747087386/asset_3_mm4slz.png",
+    },
+    {
+      label: "Yurts",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076706/asset_53_oq1fcf.png",
+    },
+    {
+      label: "Riads",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747087473/asset_47_tjfazg.png",
+    },
+    {
+      label: "Campers",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747078120/asset_56_qjkmge.png",
+    },
+    {
+      label: "Play",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747087397/asset_25_idnujj.png",
+    },
+    {
+      label: "Treehouses",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747076707/asset_5_iorm5b.png",
+    },
+    {
+      label: "Caves",
+      iconUrl:
+        "https://res.cloudinary.com/dkdlwoien/image/upload/v1747087402/asset_36_mil2ir.png",
+    },
   ]
 
   return (
     <section className="filter-carousel">
-      <button className="scroll-btn left">←</button>
+      <button className="scroll-btn left" onClick={onScrollLeft}>
+        {/* <LeftIcon /> */}
+      </button>
 
-      <div className="filter-list">
+      <div className="filter-list" ref={scrollContainerRef}>
         {filters.map((filter, idx) => (
           <div className="filter-item" key={idx}>
             <img className="icon-img" src={filter.iconUrl} alt={filter.label} />
@@ -125,7 +304,9 @@ export function StayIconFilter() {
         ))}
       </div>
 
-      <button className="scroll-btn right">→</button>
+      <button className="scroll-btn right" onClick={onScrollRight}>
+        {/* <RightIcon /> */}
+      </button>
     </section>
   )
 }
