17ba24fec3a98e63605966275cc585248f0e6a49
Nir Sultan <snufkin62@gmail.com>
2025-05-12 16:49:12 +0300

    Save before merge

--- src/cmps/StayFilterMinimized.jsx
+++ src/cmps/StayFilterMinimized.jsx
@@ -1,5 +1,12 @@
 
 
-export function StayFilterMinimized() {
-  
+export function StayFilterMinimized({ filterBy, setFilterBy }) {
+  const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
+
+
+  return (
+    <section className="filter-minimized">
+
+    </section>
+  )
 }
--- src/pages/StayIndex.jsx
+++ src/pages/StayIndex.jsx
@@ -5,6 +5,7 @@ import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { stayService } from '../services/stay/'
 import { StayList } from '../cmps/StayList'
 import { StayFilter } from '../cmps/StayFilter'
+import { StayFilterMinimized } from '../cmps/StayFilterMinimized'
 
 export function StayIndex() {
   const stays = useSelector(storeState => storeState.stayModule.stays)
@@ -49,8 +50,8 @@ export function StayIndex() {
 
   return (
     <main className="stay-index">
-      {/* <StayFilter filterBy={filterBy} setFilterBy={setFilterBy} /> */}
-      
+      <StayFilterMinimized filterBy={filterBy} setFilterBy={setFilterBy} />
+
       <StayList stays={stays} onUpdateStay={onUpdateStay} />
     </main>
   )
