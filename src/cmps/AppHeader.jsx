f313d0f67cde5849e8f8dba6b0fe4976cf0965de
Nir Sultan <snufkin62@gmail.com>
2025-05-15 00:11:46 +0300

    Add scroll observer

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { AirbnbLogoFull, AirbnbLogoIcon, MenuIcon } from "./Icons";
@@ -10,28 +10,54 @@ import { loadStays } from "../store/actions/stay.actions";
 export function AppHeader() {
   const loggedInUser = useSelector(storeState => storeState.userModule.loggedInUser)
   const [filterBy, setFilterBy] = useState(stayService.getDefaultFilter())
+  const [isAtTop, setIsAtTop] = useState(true)
+  const topRef = useRef()
 
   useEffect(() => {
     loadStays(filterBy)
   }, [filterBy])
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsAtTop(entry.isIntersecting)
+      },
+      {
+        root: null,
+        threshold: 0.1
+      }
+    )
+
+    if (topRef.current) {
+      observer.observe(topRef.current)
+    }
+
+    return () => {
+      if (topRef.current) observer.unobserve(topRef.current)
+    }
+  }, [])
+
+
 
   return (
-    <header className="app-header full ">
-      <section className='header-content main-layout'>
-        <NavLink to="/" className="logo">
-          <AirbnbLogoIcon className="logo-icon" />
-          <AirbnbLogoFull className="logo-full" />
-        </NavLink>
+    <>
+      <div className="observer-top" ref={topRef}></div>
+      <header className={`app-header full ${isAtTop ? 'header-large' : 'header-small'}`}>
+        <section className='header-content main-layout'>
+          <NavLink to="/" className="logo">
+            <AirbnbLogoIcon className="logo-icon" />
+            <AirbnbLogoFull className="logo-full" />
+          </NavLink>
 
-        <button className="btn-user">
-          <img className="user-img" src={loggedInUser?.imgUrl} alt={loggedInUser?.fullname} /></button>
-        <button className="btn-menu"><MenuIcon /></button>
-      </section>
+          <button className="btn-user">
+            <img className="user-img" src={loggedInUser?.imgUrl} alt={loggedInUser?.fullname} /></button>
+          <button className="btn-menu"><MenuIcon /></button>
+        </section>
 
-      <StayFilterExpanded filterBy={filterBy} setFilterBy={setFilterBy} />
+        <StayFilterExpanded filterBy={filterBy} setFilterBy={setFilterBy} />
 
-    </header>
+      </header>
+    </>
   )
 }
 
