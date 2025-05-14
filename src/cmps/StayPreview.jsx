eb085d70a1455ece54a916f0f349e8e021efbb86
Nir Sultan <snufkin62@gmail.com>
2025-05-14 18:50:27 +0300

    Format avg rating values

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { stayService } from '../services/stay'
 import { HeartIcon, StarIcon } from './Icons'
+import { formatRating } from '../services/util.service';
 
 export function StayPreview({ stay }) {
 
@@ -11,17 +12,17 @@ export function StayPreview({ stay }) {
         <section className="preview-img">
           <img src={stay.imgUrls[0]} alt={stay.name} />
         </section>
-        <button className='btn-like' onClick={(ev) => {ev.preventDefault(); ev.stopPropagation()}}>
-        <HeartIcon className="icon-like" />
-      </button>
-      <section className="preview-details">
-        <h2 className='header bold'>{stayService.getStayAddressStr(stay)}</h2>
-        <p className='avgRating'>{<StarIcon />}{stay.avgRating}</p>
-        <p className='summary regular'>{stay.summary}</p>
-        <p className='bed-count regular'>{stay.bedCount} bed{stay.bedCount === 1 ? '' : 's'}</p>
-        <p className='price regular'><span className='bold'>${stay.price.toLocaleString()}</span> night</p>
-      </section>
-    </Link>
+        <button className='btn-like' onClick={(ev) => { ev.preventDefault(); ev.stopPropagation() }}>
+          <HeartIcon className="icon-like" />
+        </button>
+        <section className="preview-details">
+          <h2 className='header bold'>{stayService.getStayAddressStr(stay)}</h2>
+          <p className='avgRating'>{<StarIcon />}{formatRating(stay.avgRating)}</p>
+          <p className='summary regular'>{stay.summary}</p>
+          <p className='bed-count regular'>{stay.bedCount} bed{stay.bedCount === 1 ? '' : 's'}</p>
+          <p className='price regular'><span className='bold'>${stay.price.toLocaleString()}</span> night</p>
+        </section>
+      </Link>
     </article >
   )
 }
