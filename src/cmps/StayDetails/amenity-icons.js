d88171655161f54484bfd3cbace1274385f58ee0
Shalev Azulay <shalev3238@gmail.com>
2025-05-15 20:19:53 +0300

    Add Amenities to StayDetails page

---
+++ src/cmps/StayDetails/amenity-icons.js
@@ -0,0 +1,25 @@
+import {
+  FaWifi, FaTv, FaSwimmingPool, FaParking,
+  FaSnowflake, FaUtensils, FaDog, FaSmokingBan,
+  FaHotTub, FaFireExtinguisher, FaFirstAid, FaBolt
+} from 'react-icons/fa'
+
+export const amenityIcons = {
+  'Wifi': FaWifi,
+  'TV': FaTv,
+  'Pool': FaSwimmingPool,
+  'Free parking': FaParking,
+  'AC': FaSnowflake,
+  'Air Conditioning': FaSnowflake,
+  'Kitchen': FaUtensils,
+  'Pet friendly': FaDog,
+  'No smoking': FaSmokingBan,
+  'Jacuzzi': FaHotTub,
+  'Fire extinguisher': FaFireExtinguisher,
+  'First aid kit': FaFirstAid,
+  'EV charger': FaBolt,
+  'BBQ': FaUtensils,
+  'Fireplace': FaFireExtinguisher,
+  'Lake Access': FaSwimmingPool,
+  'Coffee Maker': FaUtensils,
+}
