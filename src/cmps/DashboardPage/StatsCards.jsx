d1f33fe547cfa022f8a3b7b1f09e81ab74ba6d6f
Ido Feffer <idoppr1@gmail.com>
2025-05-17 11:11:33 +0300

    UI update dashboard

--- src/cmps/DashboardPage/StatsCards.jsx
+++ src/cmps/DashboardPage/StatsCards.jsx
@@ -10,21 +10,29 @@ export function StatsCards({ stats }) {
     {
       label: "Total sales",
       value: `$${stats.totalRevenue.toFixed(2)}`,
+      diff: -5,
+      isPositive: false,
       icon: <DashboardStatHandsIcon />,
     },
     {
       label: "Total customers",
       value: stats.totalCustomers,
+      diff: 10,
+      isPositive: true,
       icon: <DashboardTotalCustomersIcon />,
     },
     {
       label: "Refunded",
       value: stats.refunded,
+      diff: -2,
+      isPositive: false,
       icon: <DashboardRefundedIcon />,
     },
     {
       label: "Average revenue",
       value: `$${stats.avgRevenue}`,
+      diff: 5,
+      isPositive: true,
       icon: <DashboardAverageIcon />,
     },
   ]
@@ -37,7 +45,17 @@ export function StatsCards({ stats }) {
             <h4>{stat.label}</h4>
             {stat.icon}
           </div>
+
           <p className="value">{stat.value}</p>
+
+          {typeof stat.diff === "number" && (
+            <div className="diff">
+              <span className={stat.isPositive ? "positive" : "negative"}>
+                {stat.isPositive ? `+${stat.diff}%` : `${stat.diff}%`}
+              </span>{" "}
+              compared to last month
+            </div>
+          )}
         </article>
       ))}
     </section>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,45 +1,3 @@
-// import { useEffect, useState } from "react"
-// import { sellerService } from "../services/seller/index"
-// // import salesData from "../assets/maps/world.geo.json"
-
-// import { StatsCards } from "../cmps/DashboardPage/StatsCards"
-// import { DashboardOverviewSales } from "../cmps/DashboardPage/Overview"
-// import { HeatMap } from "../cmps/DashboardPage/HeatMap"
-// import { ReservationTable } from "../cmps/DashboardPage/ReserationTable"
-// import { sales } from "../services/seller/seller.service.local"
-
-// export function Dashboard() {
-//   const [stats, setStats] = useState(null)
-//    const [countryData, setCountryData] = useState([])
-
-//   useEffect(() => {
-//     sellerService.getDashboardStats().then(setStats)
-//   }, [])
-//    useEffect(() => {
-//     sellerService.getSalesByCountry().then(setCountryData)
-//   }, [])
-
-//   if (!stats) return <p>Loading...</p>
-
-//   return (
-//     <section className="dashboard-main">
-//       <h4>Review & analyze your data</h4>
-//       <section className="sales-cards">
-//         <StatsCards stats={stats} />
-//       </section>
-//       <section className="overview-chart">
-//         <DashboardOverviewSales stats={stats} />
-//       </section>
-//       <section className="heat-chart">
-//          <HeatMap dataByCountry={countryData} />
-//       </section>
-//       <section className="reservation-table">
-//         <ReservationTable sales={sales}/>
-//       </section>
-//     </section>
-//   )
-// }
-
 import { useEffect, useState } from "react"
 import { sellerService } from "../services/seller/index"
 
@@ -63,19 +21,21 @@ export function Dashboard() {
 
   return (
     <section className="dashboard-main">
-      <h4>Review & analyze your data</h4>
+      <div className="dashboard-container">
+        <h4>Review & analyze your data</h4>
 
-      <section className="sales-cards">
-        <StatsCards stats={stats} />
-      </section>
+        <section className="sales-cards">
+          <StatsCards stats={stats} />
+        </section>
 
-      <section className="overview-chart">
-        <DashboardOverviewSales stats={stats} />
-      </section>
+        <section className="overview-chart">
+          <DashboardOverviewSales stats={stats} />
+        </section>
 
-      <section className="heat-chart">
-        <HeatMap dataByCountry={countryData} />
-      </section>
+        <section className="heat-chart">
+          <HeatMap dataByCountry={countryData} />
+        </section>
+      </div>
     </section>
   )
 }
