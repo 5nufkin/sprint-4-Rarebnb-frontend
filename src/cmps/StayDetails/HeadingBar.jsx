3cb9bb1a0c9a90b8c0495e33c4042d7dac4c4a43
Shalev Azulay <shalev3238@gmail.com>
2025-05-19 04:48:09 +0300

    Add css styling to StayDetails page

--- src/cmps/StayDetails/HeadingBar.jsx
+++ src/cmps/StayDetails/HeadingBar.jsx
@@ -1,12 +1,14 @@
+import { IoMdStar } from "react-icons/io";
 export function HeadingBar({ stay }) {
   return (
     <header className="heading-bar">
       <h1>{stay.name}</h1>
       <div className="meta">
-        <span>⭐ {stay.rating ?? 4.88}</span>
-        <span> · {stay.reviews?.length ?? 0} reviews</span>
-        <button className="icon-btn">Share</button>
-        <button className="icon-btn">Save</button>
+        <span>
+          <IoMdStar size={12} />
+          {stay.avgRating ?? 4.88}  · 
+          </span>
+        <span className="rating"> {stay.reviews?.length ?? 0} reviews</span>
       </div>
     </header>
   )
