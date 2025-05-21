c25b9b53683fcea58cd807107b7b505d3ed1d7e2
Ido Feffer <idoppr1@gmail.com>
2025-05-21 21:52:00 +0300

    StayIconFilter query params

--- src/cmps/StayIconFilter.jsx
+++ src/cmps/StayIconFilter.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from 'react'
 import {
   adaptedIcon,
   aFramesIcon,
@@ -41,21 +41,201 @@ import {
   vineyardsIcon,
   windmillsIcon,
   YurtsIcon,
-} from "../../src/cmps/Icons"
+} from '../../src/cmps/Icons'
+import { loadStays } from '../store/actions/stay.actions'
 
-import { loadStays } from "../store/actions/stay.actions"
+import { useSearchParams } from 'react-router-dom'
 
 export function StayIconFilter() {
   const scrollContainerRef = useRef(null)
   const [canScrollLeft, setCanScrollLeft] = useState(false)
   const [canScrollRight, setCanScrollRight] = useState(false)
   const [selectedLabel, setSelectedLabel] = useState(null)
+  const [searchParams, setSearchParams] = useSearchParams()
+  const filters = [
+    {
+      label: 'Castels',
+      icon: castelIcon,
+    },
+    {
+      label: 'Cabins',
+      icon: cabinHomeIcon,
+    },
+    {
+      label: 'Luxe',
+      icon: luxeIcon,
+    },
+    {
+      label: 'OMG!',
+      icon: OMGIcon,
+      // img: "/pic/OMG.jpg"
+    },
+    {
+      label: 'Beachfront',
+      icon: BeachFrontIcon,
+    },
+    {
+      label: 'Mansions',
+      icon: mansionIcon,
+    },
+    {
+      label: 'Lakefront',
+      icon: lakeFrontIcon,
+    },
+    {
+      label: 'Islands',
+      icon: islandsIcon,
+    },
+    {
+      label: 'Design',
+      icon: designHomesIcon,
+    },
+    {
+      label: 'Farms',
+      icon: farmsIcon,
+    },
+    {
+      label: 'Trending',
+      icon: trendingIcon,
+    },
+    {
+      label: 'Tiny homes',
+      icon: tinyHomesIcon,
+    },
+    {
+      label: 'Tropical',
+      icon: tropicalIcon,
+    },
+    {
+      label: 'Boats',
+      icon: boatIcon,
+    },
+    {
+      label: 'Play',
+      icon: playHomeIcon,
+    },
+    {
+      label: 'Ski-in/out',
+      icon: skiInOutIcon,
+    },
+    {
+      label: 'Desert',
+      icon: desertIcon,
+    },
+    {
+      label: 'A-frames',
+      icon: aFramesIcon,
+    },
+    {
+      label: 'Vineyards',
+      icon: vineyardsIcon,
+    },
+    {
+      label: 'Arctic',
+      icon: snowIcon,
+    },
+    {
+      label: 'Rooms',
+      icon: roomIcon,
+    },
+    {
+      label: 'Caves',
+      icon: caveIcon,
+    },
+    {
+      label: 'Domes',
+      icon: domesIcon,
+    },
+    {
+      label: 'Camping',
+      icon: campingIcon,
+    },
+    {
+      label: 'New',
+      icon: newHomeIcon,
+    },
+    {
+      label: 'Towers',
+      icon: towerIcon,
+    },
+    {
+      label: 'Surfing',
+      icon: surfingIcon,
+    },
+    {
+      label: 'Containers',
+      icon: containersIcon,
+    },
+    {
+      label: 'Skiing',
+      icon: skiingIcon,
+    },
+    {
+      label: 'Windmills',
+      icon: windmillsIcon,
+    },
+    {
+      label: 'Dammusi',
+      icon: dammusiIcon,
+    },
+    {
+      label: 'Riads',
+      icon: riadsIcon,
+    },
+    {
+      label: 'Barns',
+      icon: barnsIcon,
+    },
+    {
+      label: 'Yurts',
+      icon: YurtsIcon,
+    },
+    {
+      label: 'Adapted',
+      icon: adaptedIcon,
+    },
+    {
+      label: 'Hanoks',
+      icon: hanoksIcon,
+    },
+    {
+      label: 'Golfing',
+      icon: golfIcon,
+    },
+    {
+      label: 'Minsus',
+      icon: minsusIcon,
+    },
+    {
+      label: 'Trulli',
+      icon: trulliIcon,
+    },
+  ]
+
+  useEffect(() => {
+    const labelFromUrl = searchParams.get('label')
+    if (labelFromUrl) {
+      setSelectedLabel(labelFromUrl)
+      loadStays({ label: labelFromUrl })
+    }
+  }, [searchParams])
+
+
+  useEffect(() => {
+    updateScrollButtons()
+    scrollContainerRef.current?.addEventListener('scroll', updateScrollButtons)
+    return () =>
+      scrollContainerRef.current?.removeEventListener(
+        'scroll',
+        updateScrollButtons
+      )
+  }, [])
 
   function onScrollLeft() {
-    scrollContainerRef.current?.scrollBy({ left: -500, behavior: "smooth" })
+    scrollContainerRef.current?.scrollBy({ left: -500, behavior: 'smooth' })
   }
   function onScrollRight() {
-    scrollContainerRef.current?.scrollBy({ left: 500, behavior: "smooth" })
+    scrollContainerRef.current?.scrollBy({ left: 500, behavior: 'smooth' })
   }
   function updateScrollButtons() {
     const el = scrollContainerRef.current
@@ -63,180 +243,10 @@ export function StayIconFilter() {
     setCanScrollLeft(el.scrollLeft > 0)
     setCanScrollRight(el.scrollLeft + el.clientWidth < el.scrollWidth - 1)
   }
-
-  const filters = [
-    {
-      label: "Castels",
-      icon: castelIcon,
-    },
-    {
-      label: "Cabins",
-      icon: cabinHomeIcon,
-    },
-    {
-      label: "Luxe",
-      icon: luxeIcon,
-    },
-    {
-      label: "OMG!",
-      icon: OMGIcon,
-      // img: "/pic/OMG.jpg"
-    },
-    {
-      label: "Beachfront",
-      icon: BeachFrontIcon,
-    },
-    {
-      label: "Mansions",
-      icon: mansionIcon,
-    },
-    {
-      label: "Lakefront",
-      icon: lakeFrontIcon,
-    },
-    {
-      label: "Islands",
-      icon: islandsIcon,
-    },
-    {
-      label: "Design",
-      icon: designHomesIcon,
-    },
-    {
-      label: "Farms",
-      icon: farmsIcon,
-    },
-    {
-      label: "Trending",
-      icon: trendingIcon,
-    },
-    {
-      label: "Tiny homes",
-      icon: tinyHomesIcon,
-    },
-    {
-      label: "Tropical",
-      icon: tropicalIcon,
-    },
-    {
-      label: "Boats",
-      icon: boatIcon,
-    },
-    {
-      label: "Play",
-      icon: playHomeIcon,
-    },
-    {
-      label: "Ski-in/out",
-      icon: skiInOutIcon,
-    },
-    {
-      label: "Desert",
-      icon: desertIcon,
-    },
-    {
-      label: "A-frames",
-      icon: aFramesIcon,
-    },
-    {
-      label: "Vineyards",
-      icon: vineyardsIcon,
-    },
-    {
-      label: "Arctic",
-      icon: snowIcon,
-    },
-    {
-      label: "Rooms",
-      icon: roomIcon,
-    },
-    {
-      label: "Caves",
-      icon: caveIcon,
-    },
-    {
-      label: "Domes",
-      icon: domesIcon,
-    },
-    {
-      label: "Camping",
-      icon: campingIcon,
-    },
-    {
-      label: "New",
-      icon: newHomeIcon,
-    },
-    {
-      label: "Towers",
-      icon: towerIcon,
-    },
-    {
-      label: "Surfing",
-      icon: surfingIcon,
-    },
-    {
-      label: "Containers",
-      icon: containersIcon,
-    },
-    {
-      label: "Skiing",
-      icon: skiingIcon,
-    },
-    {
-      label: "Windmills",
-      icon: windmillsIcon,
-    },
-    {
-      label: "Dammusi",
-      icon: dammusiIcon,
-    },
-    {
-      label: "Riads",
-      icon: riadsIcon,
-    },
-    {
-      label: "Barns",
-      icon: barnsIcon,
-    },
-    {
-      label: "Yurts",
-      icon: YurtsIcon,
-    },
-    {
-      label: "Adapted",
-      icon: adaptedIcon,
-    },
-    {
-      label: "Hanoks",
-      icon: hanoksIcon,
-    },
-    {
-      label: "Golfing",
-      icon: golfIcon,
-    },
-    {
-      label: "Minsus",
-      icon: minsusIcon,
-    },
-    {
-      label: "Trulli",
-      icon: trulliIcon,
-    },
-  ]
-
-  useEffect(() => {
-    updateScrollButtons()
-    scrollContainerRef.current?.addEventListener("scroll", updateScrollButtons)
-    return () =>
-      scrollContainerRef.current?.removeEventListener(
-        "scroll",
-        updateScrollButtons
-      )
-  }, [])
-
   function onLabelClick(label) {
     setSelectedLabel(label)
-    loadStays({label})
+    setSearchParams({ label })
+    loadStays({ label })
   }
 
   return (
@@ -252,7 +262,7 @@ export function StayIconFilter() {
           {filters.map((filter, idx) => (
             <div
               className={`filter-item ${
-                selectedLabel === filter.label ? "active" : ""
+                selectedLabel === filter.label ? 'active' : ''
               }`}
               key={idx}
               onClick={() => onLabelClick(filter.label)}
